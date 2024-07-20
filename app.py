706372c82f0056c77e07af13b933d9573c9d9871
SinaHosseini <sshosseinivaez@gmail.com>
2024-07-20 23:08:00 +0330

    first commit~

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+
+app = Flask("My Personal WebSite")
+
+
+@app.route("/")
+def my_root():
+    return render_template("index.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
